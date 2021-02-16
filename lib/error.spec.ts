939c3f473e73f1a69c4fdcb27d062b9826d2f82f
Gustaf Räntilä <g.rantila@gmail.com>
2021-02-16 20:52:36 +0100

    test(error): completed error tests

--- lib/error.spec.ts
+++ lib/error.spec.ts
@@ -5,6 +5,10 @@ import {
 	RelatedError,
 	throwUnsupportedError,
 	throwRelatedError,
+	isCoreTypesError,
+	decorateError,
+	decorateErrorMeta,
+	CoreTypesErrorMeta,
 } from './error'
 
 
@@ -82,6 +86,69 @@ describe( "errors", ( ) =>
 			expect( err.relatedError ).toBe( related );
 			expect( err.source ).toEqual( 'src' );
 			expect( err.filename ).toEqual( 'file' );
+		} );
+	} );
+
+	describe( "isCoreTypesError", ( ) =>
+	{
+		it( "should be true for CoreTypesErrors", ( ) =>
+		{
+			const err1 = catchError(
+				( ) => throwUnsupportedError( "foo", { type: 'string' } )
+			);
+			expect( isCoreTypesError( err1 ) ).toBe( true );
+
+			const err2 = catchError(
+				( ) => throwRelatedError( err1 )
+			);
+			expect( isCoreTypesError( err2 ) ).toBe( true );
+		} );
+
+		it( "should be false for non-CoreTypesErrors", ( ) =>
+		{
+			expect( isCoreTypesError( new Error( ) ) ).toBe( false );
+		} );
+	} );
+
+	describe( "decorateError", ( ) =>
+	{
+		it( "should", ( ) =>
+		{
+			const err = catchError(
+				( ) =>
+					throwRelatedError( new Error( 'rel' ), { source: 'src' } )
+			);
+			decorateError( err, { filename: 'file' } );
+
+			expect( err.filename ).toEqual( 'file' );
+		} );
+	} );
+
+	describe( "decorateErrorMeta", ( ) =>
+	{
+		it( "should", ( ) =>
+		{
+			const target: CoreTypesErrorMeta = {
+				blob: { blobby: true },
+				path: [ 'a' ],
+				loc: { start: { offset: 4, line: 1, column: 4 } },
+				source: 'src',
+				filename: 'file',
+			};
+			const bak = JSON.parse( JSON.stringify( target ) );
+			const out = decorateErrorMeta(
+				target,
+				{
+					blob: { blobby: false },
+					path: [ 'b' ],
+					loc: { start: { offset: 6, line: 1, column: 6 } },
+					source: 'src-data',
+					filename: 'file.x',
+				}
+			);
+
+			expect( out ).toBe( target );
+			expect( target ).toStrictEqual( bak );
 		} );
 	} );
 } );
